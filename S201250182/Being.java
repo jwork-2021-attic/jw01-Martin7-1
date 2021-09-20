27648ddcc3aaf0d67b846edff63aad297ac4aaf7
Martin7-1 <1754350460@qq.com>
2021-09-20 23:38:23 +0800

    W01 finished

---
+++ S201250182/Being.java
@@ -0,0 +1,8 @@
+package W01.S201250182;
+
+/**
+ * @author Zyi
+ */
+public abstract class Being {
+    // 物体类
+}
---
+++ S201250182/Skill.java
@@ -0,0 +1,8 @@
+package W01.S201250182;
+
+/**
+ * @author Zyi
+ */
+public enum Skill {
+    invulnerable
+}
